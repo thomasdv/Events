8107afdc41b85ae013a06a03fd2eac7008bccd06
Martin Lutonsky <martin.lutonsky@inspire.cz>
2021-01-28 12:06:37 +0100

    removed nette/reflection dependency

--- src/Events/DI/EventsExtension.php
+++ src/Events/DI/EventsExtension.php
@@ -33,7 +33,6 @@ use Nette\DI\Helpers as DIHelpers;
 use Nette\PhpGenerator\ClassType as ClassTypeGenerator;
 use Nette\PhpGenerator\Helpers as GeneratorHelpers;
 use Nette\PhpGenerator\PhpLiteral;
-use Nette\Reflection\ClassType as ClassTypeReflection;
 use Nette\Utils\Validators;
 use ReflectionProperty;
 use Symfony\Component\EventDispatcher\Event as SymfonyEvent;
@@ -389,11 +388,11 @@ class EventsExtension extends \Nette\DI\CompilerExtension
 				continue;
 			}
 
-			$this->bindEventProperties($def, ClassTypeReflection::from($class));
+			$this->bindEventProperties($def, new \ReflectionClass($class));
 		}
 	}
 
-	protected function bindEventProperties(Definition $def, ClassTypeReflection $class)
+	protected function bindEventProperties(Definition $def, \ReflectionClass $class)
 	{
 		/** @var \Nette\DI\Definitions\ServiceDefinition $def */
 		$def = $def instanceof FactoryDefinition ? $def->getResultDefinition() : $def;
@@ -404,7 +403,7 @@ class EventsExtension extends \Nette\DI\CompilerExtension
 				continue;
 			}
 
-			if ($property->hasAnnotation('persistent') || $property->hasAnnotation('inject')) { // definitely not an event
+			if (self::propertyHasAnnotation($property, 'persistent') || self::propertyHasAnnotation($property, 'inject')) { // definitely not an event
 				continue;
 			}
 
@@ -413,9 +412,7 @@ class EventsExtension extends \Nette\DI\CompilerExtension
 					[$class->getName(), $name],
 					new PhpLiteral('$service->' . $name),
 					NULL,
-					$property->hasAnnotation('globalDispatchFirst')
-						? (bool) $property->getAnnotation('globalDispatchFirst')
-						: $this->loadedConfig['globalDispatchFirst'],
+					self::propertyHasAnnotation($property, 'globalDispatchFirst') ?: $this->loadedConfig['globalDispatchFirst'],
 				]),
 			]);
 		}
@@ -489,7 +486,7 @@ class EventsExtension extends \Nette\DI\CompilerExtension
 			throw new \InvalidArgumentException('Given class cannot be NULL');
 		}
 
-		$instance = ClassTypeReflection::from($class)->newInstanceWithoutConstructor();
+		$instance = (new \ReflectionClass($class))->newInstanceWithoutConstructor();
 		if (!$instance instanceof EventSubscriber) {
 			throw new \Kdyby\Events\UnexpectedValueException(sprintf('The class %s does not implement %s', $class, EventSubscriber::class));
 		}
@@ -497,4 +494,10 @@ class EventsExtension extends \Nette\DI\CompilerExtension
 		return $instance;
 	}
 
+	private static function propertyHasAnnotation(ReflectionProperty $property, string $annotation): bool
+	{
+		$comment = $property->getDocComment();
+
+		return (bool) \strpos($comment, "@$annotation");
+	}
 }
