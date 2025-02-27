75a410631e51883bdb531637ab61558311d8b4ba
thomasdv <thomas.dvorsky@gmail.com>
2025-02-27 22:14:25 +0100

    Update Panel.php

--- src/Events/Diagnostics/Panel.php
+++ src/Events/Diagnostics/Panel.php
@@ -412,7 +412,7 @@ class Panel implements \Tracy\IBarPanel
 			return $this->registeredClasses;
 		}
 
-		$refl = new ReflectionProperty(DIContainer::class, 'types');
+		$refl = new ReflectionProperty(DIContainer::class, 'aliases');
 		$refl->setAccessible(TRUE);
 		$types = $refl->getValue($this->sl);
 
